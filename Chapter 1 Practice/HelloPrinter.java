89b5ead16b7df0735cfcea1b0903dea32cce20c7
nathanloo9927 <ngloo@NN123-HA-03.HS.sd203.org>
2015-08-25 15:06:59 -0500

    uhhh

--- Chapter 1 Practice/HelloPrinter.java
+++
@@ -1,9 +0,0 @@
-public class HelloPrinter
-{
-    public static void main( String[] args )
-    {
-        // display a greeting in the console window
-        
-        System.out.println( "Hello, world!" );
-    }
-}
