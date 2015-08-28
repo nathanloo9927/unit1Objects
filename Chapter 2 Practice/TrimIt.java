471bc05a078983258e341cc2261eb4516bdad269
nathanloo9927 <ngloo@NN123-HA-03.HS.sd203.org>
2015-08-28 15:08:17 -0500

    yay

---
+++ Chapter 2 Practice/TrimIt.java
@@ -0,0 +1,13 @@
+public class TrimIt
+{
+   public static void main(String[] args)
+   {
+      String sentence1 = "      abc   ";
+      String sentence2 = "   def";
+      String sentence3 = "ghi         ";
+      String message = "hi";
+      message = sentence1.trim() + sentence2.trim() + sentence3.trim();
+
+      System.out.println(message);
+   }
+}
