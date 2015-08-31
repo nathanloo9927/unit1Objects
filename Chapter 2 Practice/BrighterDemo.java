afcacebeedf49a0bd70d9cd133269ac691d9bed7
nathanloo9927 <ngloo@NN123-HA-03.HS.sd203.org>
2015-08-31 15:08:18 -0500

    arghh

---
+++ Chapter 2 Practice/BrighterDemo.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class BrighterDemo
+{
+    public static void main(String[] args)
+    {
+        //JFrame frame = new JFrame();
+        //frame.setSize(200, 200);
+        //Color myColor = ...;
+        //frame.get
+    }
+}
