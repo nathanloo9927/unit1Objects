a08efaf472825eeb2b84797a26db90f9316a678e
nathanloo9927 <ngloo@NN123-HA-03.HS.sd203.org>
2015-09-03 14:58:13 -0500

    Hooray

--- TurtleLab/Turtle1.java
+++ TurtleLab/Turtle1.java
@@ -5,7 +5,35 @@ public class Turtle1
     public static void main(String[] args)
     {
         World turtleworld = new World();
-        Turtle turtle = new Turtle(turtleworld);
-        turtle.setPenColor(Color.BLACK);
+        Turtle turtle1 = new Turtle(turtleworld);
+        turtle1.penUp();
+        turtle1.forward(100);
+        turtle1.setPenColor(Color.BLACK);
+        turtle1.penDown();
+        turtle1.turn(-165);
+        turtle1.forward(200);
+        turtle1.turn(-140);
+        turtle1.forward(200);
+        turtle1.turn(-145);
+        turtle1.forward(200);
+        turtle1.turn(-145);
+        turtle1.forward(200);
+        turtle1.turn(-145);
+        turtle1.forward(210);
+        Turtle turtle2 = new Turtle(turtleworld);
+        turtle2.penUp();
+        turtle2.forward(100);
+        turtle2.setPenColor(Color.BLUE);
+        turtle2.penDown();
+        turtle2.turn(180);
+        turtle2.forward(200);
+        turtle2.turn(180);
+        turtle2.penUp();
+        turtle2.forward(100);
+        turtle2.turnRight();
+        turtle2.forward(100);
+        turtle2.penDown();
+        turtle2.turn(180);
+        turtle2.forward(200);
     }
 }
